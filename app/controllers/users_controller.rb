0c815417244fc6854f7b5fce9152e404e9e7bdf9
Mirbek Askerov <mirbek.askerov@gmail.com>
2018-04-14 13:46:30 +0600

    Refactoring views style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Messi',
+        username: '@messi',
+        avatar_url: 'http://secure.gravatar.com/avatar'
+      ),
+      User.new(
+        id: 2,
+        name: 'Ronaldu',
+        username: '@ronaldu'
+      )
+    ]
   end
 
   def new
@@ -16,5 +29,7 @@ class UsersController < ApplicationController
     )
 
     @questions = [Question.new(text: 'Whats up?', created_at: Date.parse('13.04.2018'))]
+
+    @new_question = Question.new
   end
 end
