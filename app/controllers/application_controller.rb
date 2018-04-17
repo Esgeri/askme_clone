30d2cd54bd4fb85fe65f052d00b89637e19db81c
esgeri.github.com <mirbek.askerov@gmail.com>
2018-04-18 03:14:37 +0600

    Add authorization for user and access rights

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   before_action :set_locale
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def reject_user
+    redirect_to root_path, alert: 'Вам сюда нельзя!'
   end
 end
