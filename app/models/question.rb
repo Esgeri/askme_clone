2df69285527886ce7d0db170060d17e5f7d20b3f
esgeri.github.com <mirbek.askerov@gmail.com>
2018-04-21 05:28:22 +0600

    The questions assigned by an authorized user have the author on the view page

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
+  belongs_to :questioning_user, class_name: 'User', optional: true
 
   validates :text, :user, presence: true
   validates :text, length: { maximum: 255 }
