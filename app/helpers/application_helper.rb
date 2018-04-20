4cb51db5d60df25870074f219496aa0c3e890166
esgeri.github.com <mirbek.askerov@gmail.com>
2018-04-21 00:06:10 +0600

    Add gem 'russian' and settings right question counter localization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,14 @@ module ApplicationHelper
     I18n.translate('question', count: @questions.count)
   end
 
+  def answers_count
+    I18n.translate('answer', count: @questions.where.not(answer: nil).count)
+  end
+
+  def unanswered_count
+    I18n.translate('unanswered', count: @questions_count - @answers_count)
+  end
+
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
