951e4c7c15fa5aabef882d09da459f2b6fa7b3a7
esgeri.github.com <mirbek.askerov@gmail.com>
2018-05-15 00:57:34 +0600

    Fix security bug on routes, add shortcut routes, and refactoring on views (@)

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,9 @@ Rails.application.routes.draw do
 
   get "show" => "user#show"
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :users
   resources :questions, except: [:show, :new, :index]
 
   get '/questions/hashtag/:name', to: 'questions#hashtags'
-
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
